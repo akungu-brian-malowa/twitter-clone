10734a9b2e2633fc87c82bb87b2c54d4539e478c
akungu-brian-malowa <bramalowa@gmail.com>
2023-04-21 10:24:04 -0400

    added some pictures

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -31,17 +31,34 @@ const Feed = () => {
       <TweetBox />
       {/* post */}
       <Post 
+      text="id = (param, param)"
+      avatar="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3cMuYItBmMt6A6n8h9Zl5blCKINeOV2dBcQ&usqp=CAU"/>
+      <Post 
+      text="Awsome twiter clone...."
+      avatar="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT6iwvTqMQh2pR9TJ5jGcQ-0t_5kPpsASyCag&usqp=CAU"/>
+      <Post 
       text="Awsome twiter clone...."
       avatar="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcREoJfj_I545bjYfGDKt9MUQXZaRAdolVYbBg&usqp=CAU"/>
-      <Post/>
-      <Post/>
-      <Post/>
-      <Post/>
-      <Post/>
-      <Post/>
-      <Post/>
-      <Post/>
-      <Post/>
+      <Post 
+      text="Awsome twiter clone...."
+      avatar="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRv331uX7oim1tkzDAqMcKH_nG9hNys6WV3eQ&usqp=CAU"/>
+      <Post 
+      text="Awsome twiter clone...."
+      avatar="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcREoJfj_I545bjYfGDKt9MUQXZaRAdolVYbBg&usqp=CAU"/>
+      <Post 
+      text="Awsome twiter clone...."
+      avatar="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcREoJfj_I545bjYfGDKt9MUQXZaRAdolVYbBg&usqp=CAU"/>
+      <Post 
+      text="Awsome twiter clone...."
+      avatar="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcREoJfj_I545bjYfGDKt9MUQXZaRAdolVYbBg&usqp=CAU"/>
+      <Post 
+      text="In a land of Myth at a TIME OF MAGIC....."
+      avatar="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRO-hlLa1mnf2V1p__dFMxkNjf44wHphOxH2g&usqp=CAU"/>
+      <Post 
+      text="Good mornig to you all..."
+      avatar="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ6ItzbDI0AhbD0tWSJ36z3Ih1Eg4sH_p9YAw&usqp=CAU"/>
+      
+    
     </div>
   )
 }
