7d53149734d5886719d9c95b09792027f597bca0
akungu-brian-malowa <bramalowa@gmail.com>
2023-07-28 19:04:50 -0400

    added responsivnes

--- src/components/TweetBox.js
+++ src/components/TweetBox.js
@@ -1,30 +1,28 @@
-import { Avatar, Button } from '@material-ui/core'
-import React from 'react'
-import { UserAuth } from '../context/AuthContext'
-import "./TweetBox.css"
+import { Avatar, Button } from "@material-ui/core";
+import React from "react";
+import { UserAuth } from "../context/AuthContext";
+import "./TweetBox.css";
 
 const TweetBox = () => {
-    const {user} = UserAuth();
+  const { user } = UserAuth();
   return (
     <div className="tweetBox">
-        <form>
-            <div className="tweetBox__input">
-            <div className="tweetBox__avatar">
-              <img src={user?.photoURL} alt=""/>
-            </div>
-                <input placeholder="whats happening" type="text"/>
-            </div>
-            <input 
-             className="tweetBox__imageInput"
-             placeholder="Optional: Enter image URL"
-             type="text"/>
-            <Button className="tweetBox__tweetButton">
-                Tweet
-            </Button>
-        </form>
-
+      <form>
+        <div className="tweetBox__input">
+          <div className="tweetBox__avatar">
+            <img src={user?.photoURL} alt="" />
+          </div>
+          <input placeholder="whats happening" type="text" />
+        </div>
+        <input
+          className="tweetBox__imageInput"
+          placeholder="Optional: Enter image URL"
+          type="text"
+        />
+        <Button className="tweetBox__tweetButton">Tweet</Button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default TweetBox
+export default TweetBox;
